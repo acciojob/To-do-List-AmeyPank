fbf89faf5187b398ddb4b8af9a0af324f50d58d2
AmeyPank <pankantiameya@gmail.com>
2023-02-03 11:21:22 +0530

    todo working

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 //your code here
+// let addTodoButton = document.getElementById('addTodoBtn');
 
+// let todolist = document.getElementById('todoList');
+// let inputField = documnet.getElementById('newTodoInput');
+
+let title  = newTodoInput.value 
+addTodoBtn.addEventListener('click', (e)=>{
+    e.preventDefault();
+    title.innerHTML = (todoList);
+    title = "";
+})
